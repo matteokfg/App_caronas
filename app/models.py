c793466655a2b7d3789071d386b86e56f9ac7093
matteokfg <67129306+matteokfg@users.noreply.github.com>
2022-07-21 16:02:29 -0300

    Model Usuario, Motorista e Carona

--- app/models.py
+++ app/models.py
@@ -1,37 +1,42 @@
 from django.conf import settings
 from django.db import models
-from django.forms import EmailField
 from django.utils import timezone
+import localflavor
 
 #<---------------------------------- model user ------------------------------------------>
 
 class Usuario(models.Model):
 
-    cpf_user = models.PositiveBigIntegerField(
-        MaxValueValidator=10000000000,
-        unique=True,
-        one_to_many=True,
+
+    cpf_user = localflavor.br.models.BRCPFField(
         verbose_name="CPF",
-        help_text="Coluna com CPF do usuario"
+        help_text="Coluna com CPF do usuario",
     )
+    # cpf_user = models.PositiveBigIntegerField(
+    #     MaxValueValidator=10000000000,
+    #     unique=True,
+    #     one_to_many=True,
+    #     verbose_name="CPF",
+    #     help_text="Coluna com CPF do usuario"
+    # )
 
     nome_user = models.CharField(
         max_length=100,
         verbose_name="Nome",
-        help_text="Coluna com o nome do usuario"
+        help_text="Coluna com o nome do usuario",
     )
 
     email_user = models.EmailField(
         max_length=254,
         verbose_name="Email",
-        help_text="Coluna com o email do usuario"
+        help_text="Coluna com o email do usuario",
     )
 
 
     RELATION_WITH_UNISO = (
         ('A', 'Aluno'),
         ('F', 'Funcionario'),
-        ('T', 'Terceiro')
+        ('T', 'Terceiro'),
     )
 
 
@@ -39,7 +44,7 @@ class Usuario(models.Model):
         max_length=1,
         choices=RELATION_WITH_UNISO,
         verbose_name="Relacao UNISO",
-        help_text="Coluna com a relacao do usuario com a UNISO"
+        help_text="Coluna com a relacao do usuario com a UNISO",
     )
 
 
@@ -52,64 +57,94 @@ class Usuario(models.Model):
         max_length=1,
         choices=GENERO,
         verbose_name="Genero",
-        help_text="Coluna com o genero do usuario"
+        help_text="Coluna com o genero do usuario",
     )
 
     #models.?   ->  senha
 
-#------------------deve ser no usuario ou uma nova coluna com apenas o motorista?--------
-
     motorista = models.BooleanField()
 
-#    def motorista_ou_carona(self,):
-#        if self.motorista:
-#            foto = models.ImageField?  -> fotos(cnh, carro, documento_uniso)
-#            placa = models.CharField(
-#                max_length=9,
-#                help_text="Coluna com a placa do carro da carona, sendo padrao: 'XXX-0000'."
-#            )   -> placa
-#             pass
-#         else:
-#             pass
-
-                #-----------------fim-------------------
-
-
+    def __str__(self):
+        return self.nome_user
 
 #<---------------------------------- fim model user -------------------------------------->
+#<---------------------------------- model motorista ------------------------------------->
 
+class Motorista(models.Model):
 
+    user_id = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE,
+        verbose_name="ID",
+        help_text="Coluna com o id do usuario que e motorista",
+    )
+
+    # if user_id.motorista:
+    #     # photo_car = models.ImageField(
+
+    #     # )       #-> fotos(cnh, carro, documento_uniso)
+
+    #     # photo_CNH = models.ImageField(
+
+    #     # )       #-> fotos(cnh, carro, documento_uniso)
+
+    #     # photo_doc = models.ImageField(
+
+    #     # )       #-> fotos(cnh, carro, documento_uniso)
+
+    #     placa = models.CharField(
+    #         max_length=9,
+    #         verbose_name="Placa",
+    #         help_text="Coluna com a placa do carro da carona, sendo padrao: 'XXX-0000'.",
+    #     )         #->   placa
+
+    def __str__(self):
+        return self.user_id
+
+#<---------------------------------- fim model motorista --------------------------------->
 #<---------------------------------- model carona ---------------------------------------->
 
-# class Carona(models.Model):
+class Carona(models.Model):
     
-#     class Lotacao(models.IntegerChoices):
-#         MAIS_UM = 1,
-#         MAIS_DOIS = 2,
-#         MAIS_TRES = 3,
-#         MAIS_QUATRO = 4,
-#         MAIS_CINCO = 5,
-#         MAIS_SEIS = 6
     
-#     author = models.ForeignKey(
-#         Usuario, 
-#         on_delete=models.CASCADE,
-#         help_text="Coluna com o motorista da carona"
-#         )
+    user_motorista = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE,
+        verbose_name="Usuario",
+        help_text="Coluna com o motorista da carona",
+        )
 
 #     # location_now =           | localizacao atual do carro
 #     # location_to =            | destino da carona
 
 #     placa = NAO PRECISA, ja vai estar conactado palo usuario(motorista)
 
-#     lotation = models.IntegerField(
-#         choices=Lotacao.choices,
-#         default=Lotacao.MAIS_TRES,
-#         help_text="Coluna com o numero da lotacao do carro da carona"
-#         )
+    class Lotacao(models.IntegerChoices):
+        MAIS_UM = 1,
+        MAIS_DOIS = 2,
+        MAIS_TRES = 3,
+        MAIS_QUATRO = 4,
+        MAIS_CINCO = 5,
+        MAIS_SEIS = 6,
+
+
+    lotation = models.IntegerField(
+        choices=Lotacao.choices,
+        default=Lotacao.MAIS_TRES,
+        verbose_name="Lotacao",
+        help_text="Coluna com o numero da lotacao do carro da carona",
+    )
+
+    date_carona = models.DateTimeField(
+        default=timezone.now,
+        verbose_name="Data",
+        help_text="Data da carona",
+    )
+
+    def __str__(self):
+        return f"Carona feita por {self.user_motorista}, na data {self.date_carona}"
 
 #<---------------------------- fim model carona ---------------------------------------->
 
 
-# --------------- lugar ainda nao terminado, n colocar na branch! --------------------
 
