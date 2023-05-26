632130014570b5b9b906ba29b76f8205e782838f
matteokfg <67129306+matteokfg@users.noreply.github.com>
2023-05-26 11:29:07 -0300

    fix: delete helpers.py

--- unisocar/settings.py
+++ unisocar/settings.py
@@ -142,7 +142,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [BASE_DIR / 'static',]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (images)
@@ -152,7 +151,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # https://learndjango.com/tutorials/django-login-and-logout-tutorial
